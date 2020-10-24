bab53850c236147d37a4edde9dc2211c6db3de16
NazarethMaria <62656847+NazarethMaria@users.noreply.github.com>
2020-10-24 17:26:56 +0100

    first commit

---
+++ models/authors.js
@@ -0,0 +1,29 @@
+const db = require('../db')();
+const COLLECTION = "authors";
+
+module.exports = () => {
+const get = async (id = null) => {
+    console.log(' inside authors model');
+    if (!id) {
+        const authors = await db.get(COLLECTION);
+        return authors;
+    }
+
+    const authors = await db.get(COLLECTION, { id });
+    return authors; 
+;}
+
+    const add = async (name) => {
+        const authorCount = await db.count(COLLECTION);
+        const results = await db.add(COLLECTION, {
+            id: authorCount + 1,
+            name: name
+        });
+        return results.result;
+    };
+
+    return {
+        get,
+        add
+    };
+};
